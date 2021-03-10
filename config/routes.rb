5d9f0dc0c61f7d370907fd2a5202efb6e42b34fb
Matheus Penchel <matheus.penchel@gmail.com>
2021-03-10 11:03:34 -0300

    Action Cable class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :chatrooms, only: :show do
+    # POST /chatrooms/:chatroom_id/messages
+    resources :messages, only: :create
+  end
+
+  # # POST /messages
+  # resources :messages, only: :create
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
